aebeb23ffc337c257e936cf18d62bcd8baa6075a
WayneTsu <w8527391@gmail.com>
2020-04-23 12:23:35 +0800

    test VR user

---
+++ Assets/Script/VRuser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRuser : MonoBehaviour
+{
+    public GameObject car;
+    public GameObject user;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newpos = car.transform.position;
+        newpos.y += 15;
+        user.transform.position = newpos;
+    }
+}
